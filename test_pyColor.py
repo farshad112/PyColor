268741e962e4195a4e13ab7104510960e4512e82
Farshad Bin Fazle <farshad1563@gmail.com>
2017-04-23 13:07:02 +0600

    Added initial version of PyColor. Unit test is not done

---
+++ test_pyColor.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+import PyColor
+import random
+
+__author__ = 'Farshad Bin Fazle'
+__project__ = 'PyColor'
+
+class TestPyColor(TestCase):
+    
+    def test_update_color(self):
+        test_color = PyColor("Yellow")
+        self.fail()
+    
+    def test_get_default_response(self):
+        self.fail()
+    
+    def test_set_default_response_mode(self):
+        self.fail()
+    
+    def test_get_rgb_hex_string(self):
+        self.fail()
+    
+    def test_get_rgb_hex_tuple(self):
+        self.fail()
+    
+    def test_get_rgba_hex_tuple(self):
+        self.fail()
+    
+    def test_list_available_colors(self):
+        self.fail()
+    
+    def test_update_color_by_string(self):
+        self.fail()
+    
+    def test_update_color_by_rgb_tuple(self):
+        self.fail()
+    
+    def test_update_color_by_rgba_tuple(self):
+        self.fail()
+    
+    def test_set_color_by_code(self):
+        self.fail()
+    
+    def test_set_color_name(self):
+        self.fail()
